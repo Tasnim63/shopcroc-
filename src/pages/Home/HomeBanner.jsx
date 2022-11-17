09589cc2455b6b7e391940b2293975e931a7368a
tasnim63 <tamanna202163@gmail.com>
2022-11-17 08:38:36 +0000

    navbar added'

--- src/pages/Home/HomeBanner.jsx
+++ src/pages/Home/HomeBanner.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-// Import Swiper React components
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import "./HomeBanner.css";
 
-// import required modules
 import { Pagination, Navigation } from "swiper";
 
 import offer from "../../assets/images/offer.png";
